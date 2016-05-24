de993acfba18f124e41e63e7833f5787c71cd55e
Gurram, Tejeshwar <Tejeshwar.Gurram@capitalone.com>
2016-05-24 12:22:21 -0400

    Day 07 pair programming

--- day_07/pair.py
+++ day_07/pair.py
@@ -1,3 +1,5 @@
+from math import floor
+
 denominations = [(.01, "penny"),
                  (.05, "nickel"),
                  (.10, "dime"),
@@ -9,3 +11,22 @@ denominations = [(.01, "penny"),
 
 
 
+
+
+currency_dict=dict(denominations)
+denom_list =currency_dict.keys()
+denom_list.sort(reverse=True)
+print denom_list
+
+def calculateChange(due, cash_received):
+    balance=cash_received-due
+    for i in denom_list:
+        if balance>=0:
+            denomination = floor(balance/i)
+            tuple_denom=(i,denomination)
+            balance=balance-i*denomination
+            print denomination , currency_dict.get(i)
+
+
+if __name__ == "__main__":
+    calculateChange(14.27, 20)
